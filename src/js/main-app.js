f79e675fd22bd7b9d5b54d19851838fff51d0ff2
ming li <mingyli34@gmail.com>
2018-03-05 20:15:07 -0800

    Add map with dots

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -1,4 +1,6 @@
 require('../scss/main.scss');
+const L = require('leaflet');
+// const fs = require('fs-extra');
 
 window.$('.icon-facebook').click((e) => {
   e.preventDefault();
@@ -23,4 +25,24 @@ function slugify(text)
     .replace(/\-\-+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')             // Trim - from start of text
     .replace(/-+$/, '');            // Trim - from end of text
+}
+
+var map = L.map('map').setView([37.871470, -122.260363], 15);
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
+
+const rackLocations = require('../data/rack_locations.json');
+for (var i = 0; i < rackLocations.length; i++) {
+    // console.log(rackLocations[i]);
+    var rack = rackLocations[i];
+    var circle = L.circle([rack.lat, rack.lon], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 10
+    }).addTo(map);
 }
